04164fc06aed2a5169995b53734b703038cad0f1
Edgar <onlygames324@outlook.com>
2021-11-10 00:05:01 -0600

    agregando la funcionalidad del login, agregue los servicios del usuario y los servicios de auntenticacion por parte de firebase para poner a funcionar el login, cabe resaltar que aun le faltan algunos detalles como que aparezca logout o el nombre del usuario que se acaba de logear

--- repositorioEscolar/src/environments/environment.prod.ts
+++ repositorioEscolar/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAmeg6vRT2ee4-CIGAqCzbOwkF-pKPsltU",
+    authDomain: "examen-angular-5a956.firebaseapp.com",
+    databaseURL: "https://examen-angular-5a956-default-rtdb.firebaseio.com",
+    projectId: "examen-angular-5a956",
+    storageBucket: "examen-angular-5a956.appspot.com",
+    messagingSenderId: "1097032961519",
+    appId: "1:1097032961519:web:bd233d50ad060fa1233a1a"
+  }
 };
